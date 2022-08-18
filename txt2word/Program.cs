515c7c16df4085eee2a54e2ef3fbdfa9f48f6aa0
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2022-08-17 21:38:25 -0400

    Add files via upload

---
+++ txt2word/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using System.IO;
+namespace txt2word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application wordApp = new Application();
+            Document wordDoc = wordApp.Documents.Add();
+            wordDoc.BuiltInDocumentProperties["Author"].Value = "txt2word by Awire9966 on github.";
+            
+            
+            StringBuilder stringBuilder = new StringBuilder();
+            using (FileStream fs = File.Open(args[0], FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                wordDoc.Content.Text = sr.ReadToEnd();
+                wordDoc.SaveAs(args[1]);
+            }
+           
+        }
+    }
+}
